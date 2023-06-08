779329500ce09f718c66f1b0c8a1bb3ebaaca765
owenarsene <118958358+owenarsene@users.noreply.github.com>
2023-06-08 18:00:14 +0800

    Update phonetics.py

--- phonetics.py
+++ phonetics.py
@@ -1,18 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 def read(word):
-    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
-
+    word = input('請輸入關鍵字:')
+    url = f'https://pansci.asia/?post_type%5B%5D=post&post_type%5B%5D=post_review&post_type%5B%5D=pan_booklist&s={word}'
     html = requests.get( url )
     bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('table', id='searchL')
-    try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
-        phone = [e.text for e in phones]
-        s = " ".join( phone )
-        # s = row.find('sub')
-        return ( chinese + s )
-    except:
-        return ( '查無此字' )
+    data = bs.find_all('a', {'class': 'post-title ga_track'})
+    
+    if len(data) > 0:
+        selected_item = random.choice(data)
+        title = selected_item.text.strip()
+        photo_url = selected_item.find_previous('img')['src']
+
+        #print("Title:", title)
+        #print("Photo URL:", https://pansci.asia/wp-content/uploads/2023/03/christian-rosswag-P_6UKCsYLNs-unsplash-510x315.jpg)
+
+        # Display the photo
+        response = (title, photo_url)
+        return ( response )
+    else:
+        return ("查無資料")
+
+        
