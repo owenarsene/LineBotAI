10da37f55bddab5af6d5abe2e4a2e1529bd912e7
owenarsene <118958358+owenarsene@users.noreply.github.com>
2023-06-13 17:09:26 +0800

    Update phonetics.py

--- phonetics.py
+++ phonetics.py
@@ -5,30 +5,31 @@ import os
 from PIL import Image
 from io import BytesIO
 word = os.getenv('KEYWORD', 'default_keyword')
+import requests
+from bs4 import BeautifulSoup
+import random
+import os
+from PIL import Image
+from io import BytesIO
+word = os.getenv('KEYWORD', 'default_keyword')
 def read(word):
     url = f'https://pansci.asia/?post_type%5B%5D=post&post_type%5B%5D=post_review&post_type%5B%5D=pan_booklist&s={word}'
     html = requests.get(url)
     bs = BeautifulSoup(html.text, 'lxml')
     data = bs.find_all('a', {'class': 'post-title ga_track'})
-
+​
     if len(data) > 0:
         selected_item = random.choice(data)
         title = selected_item.text.strip()
         photo_url = selected_item.find_previous('img')['src']
-        article_url = selected_item['href']
-
-        # Create the clickable link with the title and photo URL
-        link = f"<a href='{photo_url}'>{title}</a>"
-
-        # Create the clickable link with the article URL
-        article_link = f"<a href='{article_url}'>Read Article</a>"
-
-        # Combine the photo link, article link, and title
-        result = f"{link}<br/>{article_link}"
-
-        return result
+​
+        #print("Title:", title)
+        #print("Photo URL:", https://pansci.asia/wp-content/uploads/2023/03/christian-rosswag-P_6UKCsYLNs-unsplash-510x315.jpg)
+​
+        # Display the photo
+        response = (title, photo_url)
+        return ( response )
     else:
-        return "查無資料"
-
-result = read(word)
-print(result)
+        return ("查無資料")
+read(word)
+​
